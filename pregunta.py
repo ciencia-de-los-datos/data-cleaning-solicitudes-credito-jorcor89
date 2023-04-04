6de524f5b00291e0d8a511693b5dad479db6757c
jorcor89 <125140181+jorcor89@users.noreply.github.com>
2023-04-04 16:06:38 -0500

    Limpieza de Datos Solucion
    Intento 3

--- pregunta.py
+++ pregunta.py
@@ -12,27 +12,21 @@ import pandas as pd
 def clean_data():
 
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col=0)
-    df.reset_index(inplace=True,drop=True)
+    df = pd.read_csv("solicitudes_credito.csv", sep=";").drop(columns=['Unnamed: 0'])
+    df = df.dropna().drop_duplicates()
 
-    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'],dayfirst=True)
 
-    df.dropna(axis='index',inplace=True)
-    df.drop_duplicates(inplace=True)
+    normalize_str = lambda x: x.lower().replace('_', ' ').replace('-', ' ').strip()
+    df['sexo'] = df['sexo'].apply(normalize_str)
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].apply(normalize_str)
+    df['idea_negocio'] = df['idea_negocio'].apply(normalize_str)
+    df['línea_credito'] = df['línea_credito'].apply(normalize_str)
+    df['barrio'] = df['barrio'].apply(lambda x: x.lower().replace('_', '-').replace('-', ' '))
 
-    df['sexo'] = df['sexo'].str.lower().astype(str).str.strip()
-    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower().astype(str)
-    df['idea_negocio'] = df['idea_negocio'].str.lower().astype(str)
-    df['barrio'] = df['barrio'].str.lower().astype(str)
-    df['línea_credito'] = df['línea_credito'].str.lower().astype(str)
-        
-    df['idea_negocio'] = df['idea_negocio'].str.replace('_',' ').str.replace('-',' ').str.strip()
-    df['barrio'] = df['barrio'].str.replace('_','-').str.replace('-',' ')
-    df['línea_credito'] = df['línea_credito'].str.replace('_',' ').str.replace('-',' ').str.strip()
 
-    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',','').str.replace('$','',regex=False).str.replace(' ','').str.strip().astype(float)   
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',', '').str.replace('$', '', regex=False).str.replace(' ', '').astype(float)
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], dayfirst=True)
 
-    df.drop_duplicates(inplace=True)
-    df.dropna(axis='index',inplace=True)
-    
+    df = df.drop_duplicates().dropna()
+
     return df
